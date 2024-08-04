fc073f7bef55f347c28cfe8b27e0fe8609bbafba
MarinaYano <myano2621@gmail.com>
2024-08-04 01:49:56 -0700

    add: done with basic server settings

--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,21 @@
-console.log("Hello via Bun!");
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(
+  cors({
+    origin:
+      process.env.NODE_ENV === "development" ? "http://localhost:5173" : "",
+  })
+);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
