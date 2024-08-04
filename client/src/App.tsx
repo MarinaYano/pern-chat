4fcb49dfb76ba89bebfcfc1e2b86366cfe10b056
MarinaYano <myano2621@gmail.com>
2024-08-03 23:58:13 -0700

    add: install material ui and delete default css codes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,13 @@
+import { Button } from "@mui/material"
+
 function App() {
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
+    </>
   )
 }
 
