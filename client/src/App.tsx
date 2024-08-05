ad6e27df3ea06d1a296a49e5fb5c3cbc440cc35a
MarinaYano <myano2621@gmail.com>
2024-08-05 00:43:33 -0700

    add: create routes and layout

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,27 @@
-import { Button } from '@mui/material';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+import ColorMode from './theme/ColorMode.tsx';
+import { ColorModeContext } from './components/layout/ThemeToggle.tsx';
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import Layout from './pages/Layout.tsx';
+import Home from './pages/Home.tsx';
 
 function App() {
+  const { theme, colorMode } = ColorMode();
   return (
-    <>
-      <Button variant="contained" color="primary">
-        Hello World
-      </Button>
-    </>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+
+        <Router>
+          <Routes>
+            <Route path="/" element={<Layout />}>
+              <Route index element={<Home />} />
+            </Route>
+          </Routes>
+        </Router>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
---
+++ client/src/pages/Layout.tsx
@@ -0,0 +1,13 @@
+import { Outlet } from 'react-router-dom';
+import Navbar from '../components/layout/Navbar';
+
+const Layout = () => {
+  return (
+    <div className="flex">
+      <Navbar />
+      <Outlet />
+    </div>
+  );
+};
+
+export default Layout;
