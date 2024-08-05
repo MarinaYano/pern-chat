100548d44c96e35ee327875914ff9a5e88bee1b1
MarinaYano <myano2621@gmail.com>
2024-08-05 03:30:46 -0700

    add: create socket server

--- server/index.ts
+++ server/index.ts
@@ -1,12 +1,22 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import http from "http";
+import { Server } from "socket.io";
+import { socketHandler } from "./socket/socket";
 
 dotenv.config();
 
 const app = express();
-
+const server = http.createServer(app);
 const PORT = process.env.PORT || 3000;
+
+const io = new Server(server, {
+  cors: {
+    origin:
+      process.env.NODE_ENV === "development" ? "http://localhost:5173" : "",
+  },
+});
 
 app.use(express.json());
 app.use(
@@ -16,6 +26,8 @@ app.use(
   })
 );
 
-app.listen(PORT, () => {
+socketHandler(io);
+
+server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
