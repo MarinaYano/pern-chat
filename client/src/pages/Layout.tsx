16e457fb01e08fb3158c2637f87a3edfb6843dd6
MarinaYano <myano2621@gmail.com>
2024-08-05 03:04:45 -0700

    update: add mobile navbar for responsiveness

--- client/src/pages/Layout.tsx
+++ client/src/pages/Layout.tsx
@@ -1,10 +1,17 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from '../components/layout/Navbar';
+import { Box } from '@mui/material';
+import MobileNavbar from '../components/layout/MobileNavbar';
 
 const Layout = () => {
   return (
     <div className="flex">
-      <Navbar />
+      <Box sx={{ display: { xs: 'none', md: 'block' } }}>
+        <Navbar />
+      </Box>
+      <Box sx={{ display: { xs: 'block', md: 'none' } }}>
+        <MobileNavbar />
+      </Box>
       <Outlet />
     </div>
   );
