c36990e560280596bc83dabf8714f0d663012642
MarinaYano <myano2621@gmail.com>
2024-08-06 22:17:02 -0700

    update: remove mobile bar

--- client/src/pages/Layout.tsx
+++ client/src/pages/Layout.tsx
@@ -1,18 +1,26 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from '../components/layout/Navbar';
-import { Box } from '@mui/material';
-import MobileNavbar from '../components/layout/MobileNavbar';
+import { ErrorBoundary } from 'react-error-boundary';
+import ErrorFallback from '../components/error/ErrorFallback';
+import { ErrorInfo } from 'react';
 
 const Layout = () => {
+  const onError = (error: Error, info: ErrorInfo) => {
+    console.error('Error MEssage: ', error.message);
+    console.error('Info Component Stack: ', info.componentStack);
+  };
   return (
     <div className="flex">
-      <Box sx={{ display: { xs: 'none', md: 'block' } }}>
+      {/* <Box sx={{ display: { xs: 'none', md: 'block' } }}>
         <Navbar />
       </Box>
       <Box sx={{ display: { xs: 'block', md: 'none' } }}>
         <MobileNavbar />
-      </Box>
-      <Outlet />
+      </Box> */}
+      <Navbar />
+      <ErrorBoundary FallbackComponent={ErrorFallback} onError={onError}>
+        <Outlet />
+      </ErrorBoundary>
     </div>
   );
 };
